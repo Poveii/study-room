209099e5e6ea77617b0e749f5d9f5d8213effd7f
Poveii <yellowtutors@gmail.com>
2022-08-04 21:11:02 -0300

    Interior Consultant: Fixed somethings

--- interior-consultant/src/scripts/main.js
+++ interior-consultant/src/scripts/main.js
@@ -1,26 +1,14 @@
 const toggleButton = document.querySelector(".toggle");
+const menuButton = document.querySelector(".menu");
+const menuIcon = document.querySelector(".toggle .material-icons");
 
 function onClickMenuButton() {
   toggleButton.addEventListener("click", function () {
-    changeMenuIcon();
-    showMenu();
+    menuIcon.textContent == " menu "
+      ? (menuIcon.textContent = " close ")
+      : (menuIcon.textContent = " menu ");
+    menuButton.classList.toggle("show");
   });
-}
-
-const menuIcon = document.querySelector(".toggle .material-icons");
-
-function changeMenuIcon() {
-  if (menuIcon.textContent == " menu ") {
-    menuIcon.textContent = " close ";
-  } else menuIcon.textContent = " menu ";
-}
-
-const menuButton = document.querySelector(".menu");
-
-function showMenu() {
-  if (menuButton.classList == "menu show") {
-    menuButton.classList = "menu";
-  } else menuButton.classList.add("show");
 }
 
 onClickMenuButton();
